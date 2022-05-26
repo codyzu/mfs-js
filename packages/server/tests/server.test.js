8a1b8a58d8bb2ac92c3c3f8c63db517bd82f80b9
codyzu <cody.zuschlag@gmail.com>
2022-05-26 08:36:09 +0200

    fix tests

--- packages/server/tests/server.test.js
+++ packages/server/tests/server.test.js
@@ -1,8 +1,8 @@
-import app from '../server.js';
+import app from "../server.js";
 
 afterAll(() => app.close());
 
-test('Gets pizzas', async () => {
+test("Gets pizzas", async () => {
   const query = `query { 
     pizzaList {
       name
@@ -14,19 +14,19 @@ test('Gets pizzas', async () => {
   `;
 
   const response = await app.inject({
-    method: 'POST',
-    headers: {'content-type': 'application/json'},
-    url: '/graphql',
-    payload: JSON.stringify({query}),
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    url: "/graphql",
+    payload: JSON.stringify({ query }),
   });
 
-  const {data, errors} = await response.json();
+  const { data, errors } = await response.json();
   expect(errors).toBeUndefined();
   expect(data).toMatchInlineSnapshot(`
     Object {
       "pizzaList": Array [
         Object {
-          "name": "margarite",
+          "name": "Napolitaine",
           "toppings": Array [
             Object {
               "name": "tomato sauce",
@@ -35,24 +35,33 @@ test('Gets pizzas', async () => {
               "name": "mozzarella",
             },
             Object {
-              "name": "fresh basil",
+              "name": "fresh garlic",
+            },
+            Object {
+              "name": "olives",
+            },
+            Object {
+              "name": "anchovies",
             },
           ],
         },
         Object {
-          "name": "classico",
+          "name": "Reine",
           "toppings": Array [
             Object {
               "name": "tomato sauce",
             },
             Object {
               "name": "mozzarella",
+            },
+            Object {
+              "name": "mushrooms",
             },
             Object {
               "name": "ham",
             },
             Object {
-              "name": "mushrooms",
+              "name": "olives",
             },
           ],
         },
