f579cc8c3d33a03e30b806d9100ca5458a4becb8
codyzu <cody.zuschlag@gmail.com>
2022-06-07 13:37:25 +0200

    listen to all interfaces in prod

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,4 +1,11 @@
 import process from 'node:process';
 import app from './server.js';
+import dev from './src/dev.js';
 
-app.listen(process.env.PORT ?? 3001);
+if (dev) {
+  // Listen to the default (localhost) in dev
+  app.listen(process.env.PORT ?? 3001);
+} else {
+  // Listen to all interfaces in production
+  app.listen(process.env.PORT ?? 3001, '0.0.0.0');
+}
