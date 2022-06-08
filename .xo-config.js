3a553e616dbcb2f6d13c0da44c4786baa032d1c6
codyzu <cody.zuschlag@gmail.com>
2022-06-08 14:40:40 +0200

    cleanup

--- .xo-config.js
+++ .xo-config.js
@@ -1,32 +1,33 @@
+// eslint-disable-next-line unicorn/prefer-module
 module.exports = {
   prettier: true,
   space: true,
   ignores: ['tap-snapshots'],
   overrides: [
     {
-      files: "packages/client/**/*.jsx",
-      envs: ["es2021", "browser"],
-      extends: "xo-react",
+      files: 'packages/client/**/*.jsx',
+      envs: ['es2021', 'browser'],
+      extends: 'xo-react',
       rules: {
-        "react/react-in-jsx-scope": "off",
-        "unicorn/filename-case": [
-          "error",
+        'react/react-in-jsx-scope': 'off',
+        'unicorn/filename-case': [
+          'error',
           {
-            case: "pascalCase"
+            case: 'pascalCase',
           },
         ],
-        "node/file-extension-in-import": "off",
-        "import/extensions": "off",
+        'node/file-extension-in-import': 'off',
+        'import/extensions': 'off',
       },
     },
     {
-      files: "packages/**/*.test.js",
-      envs: ["es2021", "jest"],
+      files: 'packages/**/*.test.js',
+      envs: ['es2021', 'jest'],
     },
     {
-      files: "packages/db/**/*.js",
+      files: 'packages/db/**/*.js',
       rules: {
-        "unicorn/prefer-module": "off",
+        'unicorn/prefer-module': 'off',
       },
     },
   ],
--- packages/db/.postgratorrc.js
+++ packages/db/.postgratorrc.js
@@ -1,11 +1,13 @@
-const parse = require('pg-connection-string')
+/* eslint-disable unicorn/prefer-module */
+const parse = require('pg-connection-string');
 
-const pgConfig = parse(require('./connection-string'))
+// eslint-disable-next-line import/extensions
+const pgConfig = parse(require('./connection-string'));
 
 module.exports = {
-  "migrationPattern": "migrations/*",
-  "driver": "pg",
-  // postgrator expects username (not user)
+  migrationPattern: 'migrations/*',
+  driver: 'pg',
+  // Postgrator expects username (not user)
   username: pgConfig.user,
-  ...pgConfig
-}
+  ...pgConfig,
+};
