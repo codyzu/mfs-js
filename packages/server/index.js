faa7000f46e8e2ec095f5015bf34b79130032fb9
Cody Zuschlag <cody.zuschlag@gmail.com>
2022-09-23 16:26:05 +0200

    fix fastify deprecation warnings

--- packages/server/index.js
+++ packages/server/index.js
@@ -2,10 +2,12 @@ import process from 'node:process';
 import app from './server.js';
 import dev from './src/dev.js';
 
+const port = process.env.PORT ?? 3001;
+
 if (dev) {
   // Listen to the default (localhost) in dev
-  app.listen(process.env.PORT ?? 3001);
+  app.listen({port});
 } else {
   // Listen to all interfaces in production
-  app.listen(process.env.PORT ?? 3001, '0.0.0.0');
+  app.listen({port, host: '0.0.0.0'});
 }
